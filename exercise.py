31aeb329518a2b9e994200d02030d6bc28ce93d1
aallen417 <austin.allen417@gmail.com>
2024-09-15 16:29:23 -0400

    completes exercise 3

--- exercise.py
+++ exercise.py
@@ -82,3 +82,37 @@ def check_voting_eligibility():
 
 # Call the function
 check_voting_eligibility()
+
+
+# Exercise 3: Calculate Dog Years
+#
+# Write a Python function named `calculate_dog_years` that calculates a dog's age in dog years.
+# Fill in the logic to perform the calculation inside the function.
+#
+# Function Details:
+# - Prompt the user to enter a dog's age: "Input a dog's age: "
+# - Calculate the dog's age in dog years:
+#      - The first two years of the dog's life count as 10 dog years each.
+#      - Each subsequent year counts as 7 dog years.
+# - Print the calculated age: "The dog's age in dog years is xx."
+# - Replace 'xx' with the calculated dog years.
+#
+# Hints:
+# - Use the `input()` function to capture user input.
+# - Convert the string input to an integer using `int()`.
+# - Apply conditional logic to perform the correct age calculation based on the dog's age.
+
+def calculate_dog_years():
+    # Your control flow logic goes here
+  FIRST_YEAR = 10
+  SECOND_YEAR = 10
+  ANOTHER_YEAR = 7
+  x = input("Input a dog's age:")
+  if int(x) == 1:
+    print(f"The dog's age in dog years is {FIRST_YEAR}")
+  elif int(x) == 2:
+    print(f"The dog's age in dog years is {(FIRST_YEAR + SECOND_YEAR)}")
+  elif int(x) >= 3:
+    print(f"The dog's age in dog years is {(FIRST_YEAR + SECOND_YEAR) + ((int(x) - 2) * ANOTHER_YEAR)}")
+# Call the function
+calculate_dog_years()
